187dcc4b76c8591c19825e8395d99cf0c14a8dbe
Umpalumpa <vinicius201233@hotmail.com>
2022-11-04 12:37:01 -0300

    Update

--- app/src/main/java/com/example/james_mark2/MainActivity.java
+++ app/src/main/java/com/example/james_mark2/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.james_mark2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.james_mark2.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        //inicia a navegação nos fragments
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initNavigation();
     }
+
+    //configuração da navegação no método initNavigation
+    private void initNavigation(){
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.MainFragmentContainer);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+    }
+
 }
