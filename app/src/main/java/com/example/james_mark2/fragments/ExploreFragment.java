c73d2756d7c09fcafb72e8047020b1be7b6f1ffa
srprompt <gidifilippo8@gmail.com>
2022-12-03 18:30:46 -0300

    Duplo clique Marker do Explore e Carga de Dados Eventos

--- app/src/main/java/com/example/james_mark2/fragments/ExploreFragment.java
+++ app/src/main/java/com/example/james_mark2/fragments/ExploreFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -47,10 +48,10 @@ public class ExploreFragment extends Fragment {
 
 
     private GoogleMap googleMap;
-    private LatLng iflandia = new LatLng(-23.552485, -46.632933
-
-    );
-
+    private LatLng iflandia = new LatLng(-23.552485, -46.632933);
+    private int cont=0;
+    private String aux;
+    private String aux2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -105,11 +106,21 @@ public class ExploreFragment extends Fragment {
                 googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
                     @Override
                     public boolean onMarkerClick(@NonNull Marker marker) {
-                        String markertitle = marker.getTitle();
+                        String markerTitle = marker.getTitle();
+                        aux = markerTitle;
+                        cont++;
 
-                        Intent i = new Intent(getContext(), DetailsActivity.class);
-                        i.putExtra("title", markertitle);
-                        startActivity(i);
+                        if(cont==1 && markerTitle.equals(aux2)) {
+                            Intent i = new Intent(getContext(), DetailsActivity.class);
+                            i.putExtra("title", markerTitle);
+                            startActivity(i);
+
+                            cont=0;
+
+                        }else if(!markerTitle.equals(aux2)){
+                            cont = 0;
+                        }
+                        aux2 = aux;
                         return false;
                     }
                 });
@@ -140,22 +151,37 @@ public class ExploreFragment extends Fragment {
 
                         googleMap.clear();
                         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iflandia, 13));
-                        googleMap.addMarker(new MarkerOptions()
+                        /*googleMap.addMarker(new MarkerOptions()
                                 .position(iflandia)
                                 .title(place.getName())
-                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));*/
 
                         //----------------------------------DATABASE-----------------------------------//
-                        //cidade, nome_evento, lat, lon
+                        //id, cidade, nome_evento, categoria, lat, lon
                         String evento [][] = {
-                                                {"Piracicaba", "Esalquizada", "-22.71183088087683", "-47.634542433283045"},
-                                                {"Piracicaba", "Festa da Pamonha", "-22.717381", "-47.652016"},
-                                                {"Piracicaba", "Revoada", "-22.710303", "-47.640236"},
-                                                {"Piracicaba", "Baile de Verão", "-22.69503513557547", "-47.65183755132581"},
-                                                {"Piracicaba", "Todo mundo na piscina", "-22.69285082843774", "-47.66172175823422"},
-                                                {"Piracicaba", "IFUDE#", "-22.69339641453475", "-47.62524715958402"},
-                                                {"Passos", "Casa do Vinicius", "-20.708020", "-46.628082"}
+                                                {"1", "Piracicaba", "Curso Boviplan de Gestão Agropecuária", "Evento", "-22.715558167441234", "-47.64980722872309"},
+                                                {"2", "Piracicaba", "Porto Certo", "Gastronomia", "-22.722838397542084", "-47.65596406028102"},
+                                                {"3", "Piracicaba", "Alto do Mirante", "Turismo", "-22.713849564796504", "-47.65112820630401"},
+                                                {"4", "Piracicaba", "Zoológico", "Turismo", "-22.69503513557547", "-47.65183755132581"},
+                                                {"5", "Piracicaba", "Várzea Cup", "Evento", "-22.69285082843774", "-47.66172175823422"},
+                                                {"6", "Piracicaba", "IFSP", "TAMO AQUI","-22.69338485527287", "-47.62550358946037"},
+                                                {"7", "Piracicaba", "McDonald's", "Gastronomia","-22.729672314113348", "-47.64803649179834"},
+
+                                                {"8", "Passos", "Dianelli Massas e Doces", "Gastronomia", "-20.72101017949072", "-46.60643463679768"},
+                                                {"9", "Passos", "Kubanos Lounge e Bar", "Gastronomia", "-20.728869165727815", "-46.61007820245232"},
+                                                {"10", "Passos", "Mangutti Sorvetes", "Gastronomia", "-20.717945264365873", "-46.61157418639631"},
+                                                {"11", "Passos", "Praça da Matriz", "Turismo", "-20.718274029822144", "-46.610768428723084"},
+                                                {"12", "Passos", "Borogodó Botequim", "Evento", "-20.727067673805713", "-46.60542049988647"},
+
+                                                {"13", "São Paulo", "Museu do Futebol", "Turismo", "-23.546473639757743", "-46.665216948410844"},
+                                                {"14", "São Paulo", "Museu de Arte de São Paulo Assis Chateaubriand", "Evento", "-23.559932070172042", "-46.65634683873395"},
+                                                {"15", "São Paulo", "Ponte Estaiada", "Turismo", "-23.51843489125664", "-46.637826836676155"},
+                                                {"16", "São Paulo", "Coco Bambu - JK", "Gastronomia", "-23.58250633349179", "-46.67097893892147"},
+                                                {"17", "São Paulo", "Mercado Municipal de São Paulo", "Turismo", "-23.5415570672105", "-46.63006024472805"},
+                                                {"18", "São Paulo", "O Mágico de Oz", "Evento", "-23.55778226078768", "-46.64110133123751"}
+
                         };
+
 
                         int cont=0;
                         for(int i=0; i<evento.length;i++){
@@ -165,20 +191,38 @@ public class ExploreFragment extends Fragment {
                         }
 
                     for(int i=0; i<cont; i++){
-                        if(Objects.equals(place.getName(), evento[i][0])){
+                        if(Objects.equals(place.getName(), evento[i][1])){
                             for(int j=0; j<cont; j++){
-                                iflandia = new LatLng(Double.parseDouble(evento[j][2]), Double.parseDouble(evento[j][3]));
-                                googleMap.addMarker(new MarkerOptions()
-                                        .position(iflandia)
-                                        .title(evento[j][1])
-                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-                                );
+                                iflandia = new LatLng(Double.parseDouble(evento[j][4]), Double.parseDouble(evento[j][5]));
+                                    if (evento[j][3] == "Evento") {
+                                        googleMap.addMarker(new MarkerOptions()
+                                                .position(iflandia)
+                                                .title(evento[j][2])
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                                    }else if (evento[j][3] == "Turismo") {
+                                    googleMap.addMarker(new MarkerOptions()
+                                            .position(iflandia)
+                                            .title(evento[j][2])
+                                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                                    }else if (evento[j][3] == "Gastronomia") {
+                                        googleMap.addMarker(new MarkerOptions()
+                                                .position(iflandia)
+                                                .title(evento[j][2])
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                                    }else{
+                                        googleMap.addMarker(new MarkerOptions()
+                                                .position(iflandia)
+                                                .title(evento[j][2])
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+                                    }
+
+                                }
+
                             }
                         }
                     }
                     //--------------------------------------------------------------------------------//
 
-                    }
 
                     @Override
                     public void onError(Status status) {
